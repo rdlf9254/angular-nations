a1e94b007128becdd53706f0ff1ef65c883511d0
Rodolfo <rodolfo9254@gmail.com>
2025-03-30 17:40:23 -0300

    created home and added to routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync(),
-    providePrimeNG({
-        theme: {
-            preset: Aura
-        }
-    })
-  ]
+    provideHttpClient(),
+  ],
 };
