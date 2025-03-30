b79c716faf4d1abf4dd0e9aa95a4ddb2b1f6e1b8
Rodolfo <rodolfo9254@gmail.com>
2025-03-30 20:40:37 -0300

    better table propeties

--- src/app/components/country-table/country-table.component.ts
+++ src/app/components/country-table/country-table.component.ts
@@ -1,13 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
 @Component({
   selector: 'app-country-table',
   standalone: true,
   templateUrl: './country-table.component.html',
   styleUrls: ['./country-table.component.scss'],
-  imports: [CommonModule, TableModule],
+  imports: [
+    CommonModule,
+    TableModule,
+    ButtonModule,
+    IconFieldModule,
+    InputIconModule,
+  ],
 })
 export class CountryTableComponent {
   @Input() countries: any[] = [];
