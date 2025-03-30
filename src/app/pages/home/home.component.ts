6d04be614883d6de1be6d5fdb6bbda3526409f1c
Rodolfo <rodolfo9254@gmail.com>
2025-03-30 18:56:42 -0300

    service working in home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountryService } from '@services/country.service';
+import { Observable } from 'rxjs';
+import { NgFor, NgIf, AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgFor, NgIf, AsyncPipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
-})
-export class HomeComponent {
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
 
+})
+export class HomeComponent implements OnInit {
+  countries$!: Observable<any[]>;
+  constructor(private countryService: CountryService) {}
+  ngOnInit() {
+    this.countries$ = this.countryService.getCountries();
+  }
 }
