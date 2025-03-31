90f12e92e0cd3f00394b31787f1a06f1df471b6d
Rodolfo <rodolfo9254@gmail.com>
2025-03-30 22:23:12 -0300

    getting country code in countryDetails component

--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '@services/country.service';
 
 @Component({
   selector: 'app-country-detail',
   imports: [],
   templateUrl: './country-detail.component.html',
-  styleUrl: './country-detail.component.scss'
+  styleUrls: ['./country-detail.component.scss'],
 })
-export class CountryDetailComponent {
+export class CountryDetailComponent implements OnInit {
+  countryDetails: any;
+  countryCode: string;
 
+  constructor(
+    private route: ActivatedRoute,
+    private countryService: CountryService
+  ) {
+    this.countryCode = '';
+  }
+
+  ngOnInit() {
+    this.countryCode = this.route.snapshot.paramMap.get('code')!;
+
+    // this.countryService
+    //   .getCountryDetails(this.countryCode)
+    //   .subscribe((data) => {
+    //     this.countryDetails = data[0];
+    //   });
+  }
 }
